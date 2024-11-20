da4b9799854e052b8439abf78984231215d78e83
Baidar Gul <baidargul@outlook.com>
2024-11-20 16:35:17 +0500

    ui fixes

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -9,7 +9,7 @@ const Button = (props: Props) => {
   return (
     <button
       onClick={props.onClick}
-      className="w-full p-2 bg-gradient-to-t from-zinc-100 to-zinc-50 rounded px-4 border"
+      className="w-full p-2 bg-gradient-to-t from-zinc-100 to-zinc-50 rounded px-4 border font-semibold tracking-tight"
     >
       {props.children}
     </button>
