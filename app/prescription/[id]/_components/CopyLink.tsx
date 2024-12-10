5fa51bf1cf876ebdaa730c089f22ad52bf154da0
Baidar Gul <baidargul@outlook.com>
2024-12-10 11:48:02 +0500

    added shop context

--- app/prescription/[id]/_components/CopyLink.tsx
+++ app/prescription/[id]/_components/CopyLink.tsx
@@ -10,7 +10,7 @@ const CopyLink = (props: Props) => {
   const handlecopyLink = async () => {
     const id = props.id;
     const link = `https://gul-opticals.vercel.app/prescription/${id}`;
-    let content = `Thank you for booking your glasses with us! We’re making it easier for you by storing your prescription on our cloud server with every order. You can access your records anytime and even save prescriptions for your family members.\n\nNo more worrying about keeping track of prescription cards!\n\n ہماری سروسز استعمال کرنے کا شکریہ! ہم آپ کے لیے آسانی پیدا کر رہے ہیں، ہر آرڈر کے ساتھ آپ کی نسخہ (پریسکرپشن) کو ہمارے کلاؤڈ سرور پر محفوظ کرتے ہیں۔ آپ کسی بھی وقت اپنی معلومات تک رسائی حاصل کر سکتے ہیں اور اپنے خاندان کے افراد کے نسخے بھی محفوظ کر سکتے ہیں۔ \n\nPrescription Link: \n${link}`;
+    let content = `Thank you for booking your glasses with us! We’re making it easier for you by storing your prescription on our cloud server with every order. You can access your records anytime and even save prescriptions for your family members.\n\nNo more worrying about keeping track of prescription cards!\n\n ہماری سروسز استعمال کرنے کا شکریہ! ہم آپ کے لیے آسانی پیدا کر رہے ہیں، ہر آرڈر کے ساتھ آپ کی نسخہ (پریسکرپشن) کو ہمارے کلاؤڈ سرور پر محفوظ کرتے ہیں۔ آپ کسی بھی وقت اپنی معلومات تک رسائی حاصل کر سکتے ہیں اور اپنے خاندان کے افراد کے نسخے بھی محفوظ کر سکتے ہیں۔ \n\nPrescription Link: \n${link}\n\n\nGul Opticals, Shaheed Road, Jhang. 03438793471`;
     navigator.clipboard.writeText(content);
   };
 
